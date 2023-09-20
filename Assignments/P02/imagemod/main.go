2fdd6efe7e5bbf3827d6a8895d1ff911b532139c
Steph <25186303+aelious@users.noreply.github.com>
2023-09-20 12:58:01 -0500

    Update main.go

--- Assignments/P02/imagemod/main.go
+++ Assignments/P02/imagemod/main.go
@@ -10,23 +10,24 @@
 *  Description:
 *       This program uses an external library, gg, provided by Fogleman via github. 
 *	It generates and manipulates images in fun, funky ways. This small snippet
-*	generates a blank png containing only a black rectangle. It also includes
-*	a separate constructor that makes a copy of a pre-existing image.
+*	includesa separate constructor that makes a copy of a pre-existing image. A
+*	black rectangle is then drawn over the top of our image.
 *
 *  Usage:
-*       N/A
+*       go run .\main.go
+*	> generates an edited image of a pre-existing image via its path
 *
 *  Files:
-*       main.go     	: driver program
-*       go.mod		: necessary modules dependencies to run our go code
-*	go.sum 		: maintains checksum to keep packages from reinstalling
-*	imagemod	: package containing our image manipulator tools
-*       mustangs.png	: our created png image
-*	mustangs.jpg	: a copy of our png image stored as a jpg
-*
+*       main.go     		: driver program
+*       go.mod			: necessary modules dependencies to run our go code
+*	go.sum 			: maintains checksum to keep packages from reinstalling
+*	imagemManipulator	: contains our imagemod package and tools
+*       mustangs.png		: our created png image, now including horses :-)
+*	mustangs.jpg		: a copy of our png image stored as a jpg with a fun
+*					black rectangle drawn on it :-)
 *****************************************************************************/
 
-// imageManipulator/main.go
+// imagemod/main.go
 
 package main
 
