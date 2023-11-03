793b7521c1f3c753608e52687e80e8819336e1eb
aelious <aeliousx@gmail.com>
2023-11-03 12:19:03 -0500

    Editing functions and comments

--- Assignments/P04/main.go
+++ Assignments/P04/main.go
@@ -11,7 +11,7 @@ import (
 
 // Sequential version of the image downloader.
 func downloadImagesSequential(urls []string) {
-	// TODO: Implement sequential download logic
+	// Sequentially loops through each url and runs one at a time.
 	for i, imageUrl := range urls {
 		downloadImage(imageUrl, fmt.Sprintf("seq_image%d.png", i+1))
 	}
@@ -20,8 +20,8 @@ func downloadImagesSequential(urls []string) {
 
 // Concurrent version of the image downloader.
 func downloadImagesConcurrent(url string, i int, wg *sync.WaitGroup) {
-	// TODO: Implement concurrent download logic
-	// Decrements currently running concurrent function counter
+	// Decrements currently running concurrent function counter once the
+	// function is finished running and returns.
 	defer wg.Done()
 
 	downloadImage(url, fmt.Sprintf("con_image%d.png", i+1))
@@ -29,6 +29,8 @@ func downloadImagesConcurrent(url string, i int, wg *sync.WaitGroup) {
 }
 
 func main() {
+	// WaitGroup allows for better concurrent processing to ensure each function
+	// completes before the program ends.
 	var wg sync.WaitGroup
 	urls := []string{
 		"https://images.unsplash.com/photo-1599474401061-465ef4b5bc05?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -36,8 +38,6 @@ func main() {
 		"https://images.pexels.com/photos/1293261/pexels-photo-1293261.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
 		"https://images.unsplash.com/photo-1692003996152-59e60ea85fe5?auto=format&fit=crop&q=80&w=1935&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 		"https://cdn.pixabay.com/photo/2023/07/16/09/31/cat-8130334_960_720.jpg",
-
-		// Add more image URLs
 	}
 
 	// Sequential download
@@ -50,6 +50,7 @@ func main() {
 	for i, url := range urls {
 		// Adds to counter of how many concurrent functions are currently running
 		wg.Add(1)
+		// Initiates goroutine for each url in the url list
 		go downloadImagesConcurrent(url, i, &wg)
 	}
 	// Waits for concurrent functions to complete
@@ -59,7 +60,6 @@ func main() {
 
 // Helper function to download and save a single image.
 func downloadImage(url, filename string) error {
-	// TODO: Implement download logic
 	response, err := http.Get(url)
 	if err != nil {
 		fmt.Println("Error making the request:", err)
